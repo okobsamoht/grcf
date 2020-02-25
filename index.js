e8cbe67a3091f7c4cd82913dc6af919dc3380c2a
Thomas Boko <>
2020-02-25 16:03:25 +0100

    ok

--- index.js
+++ index.js
@@ -1,69 +1,77 @@
 #!/usr/bin/env node
-'use strict';
 
-var path = require('path');
-var fs = require('fs-extra');
+"use strict";
+
+var path = require("path");
+var fs = require("fs-extra");
 
 var componentName;
 
-var program = require('commander')
-  .version(require('./package.json').version)
-  .arguments('<component-directory>')
-  .action(function (name) {
-    componentName = name;
-  })
-  .option('-p, --pure', 'Create Pure Function Component')
-  .option('-c, --css', `Add ${componentName}.css`)
-  .parse(process.argv)
+var program = require("commander")
+    .version(require("./package.json").version)
+    .arguments("<component-directory>")
+    .action(function(name) {
+        componentName = name;
+    })
+    .option("-p, --pure", "Create Pure Function Component")
+    .option("-c, --css", `Add ${componentName}.css`)
+    .option("-t, --test", `Add ${componentName}.test.js`)
+    .parse(process.argv);
 
 createComponent(componentName);
 
 function createComponent(name) {
-  var root = path.resolve(name);
+    var root = path.resolve(name);
 
-  if (!fs.existsSync(root)) {
-    fs.mkdirSync(root);
-  }
+    if (!fs.existsSync(root)) {
+        fs.mkdirSync(root);
+    }
 
-  var cssLine = ''
-  if (program.css) {
+    var cssLine = "";
+    if (program.css) {
+        fs.writeFileSync(path.join(root, `${name}.css`));
+        cssLine = `import s from './${name}.css'\n\n`;
+    }
+
+    if (program.pure) {
+        fs.writeFileSync(
+            path.join(root, `${name}.js`),
+            `import React from 'react'\n` +
+            cssLine +
+            `const ${name} = (props) => {\n` +
+            `\treturn (\n` +
+            `\t\t<div>\n\t\t</div>\n` +
+            `\t)\n` +
+            `}\n\n` +
+            `export default ${name}`
+        );
+    } else {
+        fs.writeFileSync(
+            path.join(root, `${name}.js`),
+            `import React, { Component } from 'react'\n` +
+            cssLine +
+            `class ${name} extends Component {\n\n` +
+            `\tcomponentDidMount() {\n` +
+            `\t}\n\n` +
+            `\trender() {\n` +
+            `\t\treturn (\n` +
+            `\t\t)\n\n` +
+            `\t}\n\n` +
+            `}\n\n` +
+            `export default ${name}`
+        );
+    }
     fs.writeFileSync(
-      path.join(root, `${name}.css`),
-      `/*\n<Project Name> CSS Module\n${name}\n*/\n`
-    )
-    cssLine = `import s from './${name}.css'\n\n`
-  }
-
-  if (program.pure) {
-    fs.writeFileSync(
-      path.join(root, `${name}.js`),
-      `/*\n<Project Name> Component\n${name}\n*/\n` +
-      `import React from 'react'\n` +
-      cssLine +
-      `const ${name} = (props) => {\n` +
-        `\treturn (\n` +
-          `\t\t<div>\n\t\t</div>\n` +
-        `\t)\n` +
-      `}\n\n` +
-      `export default ${name}`
-    )
-  } else {
-    fs.writeFileSync(
-      path.join(root, `${name}.js`),
-      `/*\n<Project Name> Component\n${name}\n*/\n` +
-      `import React, { Component } from 'react'\n` +
-      cssLine +
-      `class ${name} extends Component {\n\n` +
-        `\tcomponentDidMount() {\n` +
-        `\t}\n\n` +
-        `\trender() {\n` +
-          `\t\treturn (\n` +
-          `\t\t)\n\n` +
-        `\t}\n\n` +
-      `}\n\n` +
-      `export default ${name}`
-    )
-  }
-
-  console.log(`Component ${name} created`);
+        path.join(root, `${name}.test.js`),
+        `import React from 'react'\n` +
+        `import { render } from '@testing-library/react'\n` +
+        `import ${name} from './${name}'\n` +
+        `describe('<${name} />', () => {\n` +
+        `\ttest('renders', () => {\n` +
+        `\tconst wrapper = shallow( < ${name} / > );\n` +
+        `\texpect(wrapper).toMatchSnapshot();\n` +
+        `\t});\n` +
+        `});\n`
+    );
+    console.log(`Component ${name} created`);
 }
