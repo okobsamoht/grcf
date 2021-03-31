d05b2b1aebcc21fcd6a3a9a273458b776f074af1
Thomas Boko <thomas@right-com.com>
2021-03-31 18:10:28 +0100

    updates depencies, changed command to grcf, added index.js generation

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 "use strict";
 
-var path = require("path");
-var fs = require("fs-extra");
+const path = require("path");
+const fs = require("fs-extra");
 
-var componentName;
+let componentName;
 
-var program = require("commander")
+const program = require("commander")
     .version(require("./package.json").version)
     .arguments("<component-directory>")
-    .action(function(name) {
+    .action(function (name) {
         componentName = name;
     })
     .parse(process.argv);
@@ -18,43 +18,44 @@ var program = require("commander")
 createComponent(componentName);
 
 function createComponent(name) {
-    var root = path.resolve(name);
+    let root = path.resolve(name);
 
     if (!fs.existsSync(root)) {
         fs.mkdirSync(root);
     }
 
-    var cssLine = "";
     fs.writeFileSync(
         path.join(root, `${name}.css`),
-        `.${name} {display:block}\n #${name} {display:block}`
+        `.${name} {display:block}`
     );
-    cssLine = `import s from './${name}.css'`;
 
     fs.writeFileSync(
         path.join(root, `${name}.js`),
-        `import React from 'react';
-        import './${name}.css';\n
-        function ${name}() {
-        \treturn (
-        \t\t<div className="${name}" id="${name}">
-        \t\t\t ${name}
-        \t\t</div>
-        \t);
-        };\n
-        export default ${name};\n
-        `
+`import React from 'react';
+import './${name}.css';\n
+const ${name} = (props) => {
+\treturn (
+\t\t<div className="${name}">
+\t\t\t ${name}
+\t\t</div>
+\t);
+};\n
+export default ${name};`
+    );
+    fs.writeFileSync(
+        path.join(root, `index.js`),
+        `export {default} from './${name}';`
     );
     fs.writeFileSync(
         path.join(root, `${name}.test.js`),
-        `import React from 'react';
-        import { render } from '@testing-library/react';\n
-        import ${name} from './${name}';\n
-        test('renders learn react link', () => {\n
-        \tconst { getByText } = render(<${name} />);\n
-        \tconst linkElement = getByText(/${name}/i);\n
-        \texpect(linkElement).toBeInTheDocument();\n
-        });\n`
+`import React from 'react';
+import { render } from '@testing-library/react';
+import ${name} from './${name}';\n
+test('verify component', () => {
+\tconst { getByText } = render(<${name} />);
+\tconst linkElement = getByText(/${name}/i);
+\texpect(linkElement).toBeInTheDocument();
+});\n`
     );
     console.log(`Component ${name} created`);
 }
