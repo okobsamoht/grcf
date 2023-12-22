8afcbc0b4d651ee6d4ec329a5299f92c6b654e15
Thomas Boko <thomas@right-com.com>
2023-12-22 11:37:25 +0100

    add native components

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let componentType;
 let componentStyle;
 let componentMode;
 let componentTest;
+let componentNative;
 
 const program = require("commander")
     .version(require("./package.json").version)
@@ -21,18 +22,20 @@ const program = require("commander")
     .option('-file', 'component single file mode')
     .option('-nostyle', 'component no style file')
     .option('-notest', 'component no test file')
+    .option('-native', 'component react-native type')
     .action(function (name, options) {
         componentName = name;
         componentType = options.Jsx ? 'jsx' : options.Tsx ? 'tsx' : 'js';
         componentStyle = options.Nostyle ? 'nostyle' : options.Scss ? 'scss' : 'css';
         componentMode = options.File ? 'file' : 'folder';
         componentTest = options.Notest ? 'notest' : 'test';
+        componentNative = options.Native ? 'native' : 'web';
     })
     .parse(process.argv);
 
 console.log('GRCF ' + program.version())
 
-function createComponentFolder(name, type, style, mode, test) {
+function createComponentFolder(name, type, style, mode, test, native) {
     let root;
 
     if (mode === 'folder'){
@@ -45,19 +48,36 @@ function createComponentFolder(name, type, style, mode, test) {
         fs.mkdirSync(root);
     }
 
-    if (style !== 'nostyle'){
+    if (style !== 'nostyle' && native === 'web'){
         fs.writeFileSync(
             path.join(root, `${name}.${style}`),
             `.${name} {}`);
     }
 
-    fs.writeFileSync(
-        path.join(root, `${name}.${type}`),
-        `import React from "react";\n`
-            .concat(style !== 'nostyle' ? `import "./${name}.${style}";\n` :``)
-            .concat(`const ${name} = (props${type==='tsx' ? ':any':''}) => {\n\treturn (\n\t\t<div className=\{"${name}"\}>\n\t\t\t ${name}\n\t\t</div>\n\t);\n};\n`)
-            .concat(`export default ${name};`)
-    );
+    if (style !== 'nostyle' && native === 'native'){
+        fs.writeFileSync(
+            path.join(root, `${name}.style.${type}`),
+            `import { StyleSheet } from 'react-native';\nconst ${name}Style = StyleSheet.create({\n\tview:{\n\t\tflex:1\n\t}\n});\nexport default ${name}Style;`);
+    }
+
+    if (native === "web"){
+        fs.writeFileSync(
+            path.join(root, `${name}.${type}`),
+            `import React from "react";\n`
+                .concat(style !== 'nostyle' ? `import "./${name}.${style}";\n` :``)
+                .concat(`const ${name} = (props${type==='tsx' ? ':any':''}) => {\n\treturn (\n\t\t<div className=\{"${name}"\}>\n\t\t\t ${name}\n\t\t</div>\n\t);\n};\n`)
+                .concat(`export default ${name};`)
+        );
+    }
+    if (native === "native"){
+        fs.writeFileSync(
+            path.join(root, `${name}.${type}`),
+            `import {View} from "react-native";\n`
+                .concat((style !== 'nostyle' && native === 'native') ? `import ${name}Style from "./${name}.style.${type}";\n` :``)
+                .concat(`const ${name} = (props) => {\n\treturn (\n\t\t<View style=\{${name}Style\}>\n\t\t\t ${name}\n\t\t</View>\n\t);\n};\n`)
+                .concat(`export default ${name};`)
+        );
+    }
 
     if (mode === 'folder'){
         fs.writeFileSync(
@@ -78,4 +98,4 @@ function createComponentFolder(name, type, style, mode, test) {
 }
 
 
-createComponentFolder(componentName, componentType, componentStyle, componentMode, componentTest);
+createComponentFolder(componentName, componentType, componentStyle, componentMode, componentTest, componentNative);
